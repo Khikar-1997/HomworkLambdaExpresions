fa2d7c95d13ba3fabb93977ec4e2967ff5dd064d
Khikar-1997 <57430976+Khikar-1997@users.noreply.github.com>
2020-02-05 01:06:53 -0800

    4_6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        NumericFunc numericFunc = ((number) -> {
-            int numberFactorial = 1;
-            for (int i = 1; i <= number; i++) {
-                numberFactorial *= i;
-            }
-            return numberFactorial;
-        });
-        System.out.println(numericFunc.func(5));
+        MyTest myTest = ((number) -> number >= 10 && number <= 20);
+        System.out.println(myTest.testing(15));
     }
 }
+
